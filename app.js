de4e57bf827b1569f423ca149792d95f61e95cd2
Adewale Olaoye <adewaleolaoye21@gmail.com>
2018-12-03 07:52:13 +0100

    config the MVC and routes folder

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 // initialize our express app
 const app = express();
 
+
+
 let port = 1234;
 app.listen(port, () => {
     console.log('Server is up and running on ' + port);
